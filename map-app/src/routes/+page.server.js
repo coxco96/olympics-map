d2aba6a50fb7d2ba98579c0bd1def16570fb0d41
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-07-17 19:53:24 -0400

    load data from google sheet

---
+++ map-app/src/routes/+page.server.js
@@ -0,0 +1,8 @@
+import * as db from '$lib/api/sheet.js';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+	return {
+		post: await db.getSheetData(params.slug)
+	};
+}
