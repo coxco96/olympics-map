f07892916037806a5fe3942451729877824940d4
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-07-18 16:36:51 -0400

    bind basemap year and find most relevant one

--- map-app/src/routes/+page.svelte
+++ map-app/src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script>
     export let data;
     import Map from "./Map.svelte";
-    import { Container, Col, Row, Input } from "@sveltestrap/sveltestrap";
+    import {yearsArray} from '../lib/utils/arrays.js';
+    import { Container, Col, Row, InputGroup, Input} from "@sveltestrap/sveltestrap";
     // console.log(data);
+    $: selectedYear = "All years (1896-2024)";
+
+    
+    // need to convert this data into something that can filter only to the year
 </script>
 
 <main>
@@ -13,38 +18,62 @@
                 <p class="lead">Medals won etc etc</p>
             </Col>
         </Row>
-        <Row class='mb-3'>
-            <Col>
-                <Input type="select" placeholder="Select Sport">
-                    <option value="alpha">All sports</option>
-                    <option value="bravo">Bravo</option>
-                    <option value="charlie">Charlie</option>
-                </Input>
-            </Col>
-            <Col>
-                <Input type="select" placeholder="Select Year">
-                    <option value="alpha">All Years (1886-Present)</option>
-                    <option value="bravo">Bravo</option>
-                    <option value="charlie">Charlie</option>
-                </Input>
-            </Col>
+        <Row class="mb-3">
+            <InputGroup>
+                <Col>
+                    <!-- TODO: check form accessibility here -->
+
+                    <!-- include size="2" in select node for scroll box instead of dropdown -->
+                    <select bind:value={selectedYear} class="form-select" name="Year">
+                        {#each yearsArray.reverse() as year}
+                            {#if year == "1906 (intercalary games)" || year == "1916 (not held)" || year == "1940 (not held)" || year == "1944 (not held)"}
+                                <option
+                                    class="s-y_bCXRrkrYfP"
+                                    disabled="True"
+                                    value={year}>{year}</option
+                                >
+                            {:else}
+                                <option value={year}
+                                    >{year}</option
+                                >
+                            {/if}
+                        {/each}
+                    </select>
+                </Col>
+                <Col>
+                    <select class="form-select" name="Sport">
+                        {#each yearsArray.reverse() as year}
+                            <option
+                                disabled="True"
+                                value={year}>{year}</option
+                            >
+                        {/each}
+                    </select>
+                </Col>
+            </InputGroup>
         </Row>
         <Row>
             <Col>
-                <Map />
+                <Map {selectedYear}/>
             </Col>
         </Row>
         <Row>
-            <footer class='footer'>
+            <footer class="footer">
                 <figure class="text-end">
                     <blockquote class="blockquote">
-                      <p>I was built this way for a reason, so I'm going to use it.</p>
+                        <p>
+                            I was built this way for a reason, so I'm going to
+                            use it.
+                        </p>
                     </blockquote>
                     <figcaption class="blockquote-footer">
-                     <cite title="Simone Biles">Simone Biles</cite>
+                        <cite title="Simone Biles">Simone Biles</cite>
                     </figcaption>
-                  </figure>
+                </figure>
             </footer>
         </Row>
     </Container>
 </main>
+
+<style>
+</style>
--- map-app/src/routes/Map.svelte
+++ map-app/src/routes/Map.svelte
@@ -1,9 +1,29 @@
 <script>
     import { onMount } from "svelte";
     import maplibregl from "maplibre-gl";
+    import { baseMapYears } from "../lib/utils/arrays.js";
     import "maplibre-gl/dist/maplibre-gl.css";
-    import geoData from '$lib/geojsons/world_1880.json';
+    import geoData from "$lib/geojsons/world_1880.json";
+    export let selectedYear;
 
+    // return the most recent basemap year
+    const getBaseMapYear = (selection) => {
+        for (let i = baseMapYears.length - 1; i >= 0; i--) {
+            if (selection >= baseMapYears[i]) {
+                return baseMapYears[i].toString();
+            }
+        }
+        return baseMapYears[0].toString(); // Return the first year if selection is less than the smallest year
+    };
+    // get numeric form of selectedYear
+    $: numericSelectedYear = Number(selectedYear.substring(0, 4));
+
+    $: console.log("selectedYear: ", selectedYear);
+    $: console.log("numericSelectedYear: ", numericSelectedYear);
+    $: baseMapYear = isNaN(numericSelectedYear)
+        ? 2000
+        : getBaseMapYear(numericSelectedYear);
+    $: console.log("baseMapYear: ", baseMapYear);
 
     onMount(() => {
         const map = new maplibregl.Map({
@@ -13,20 +33,19 @@
             zoom: 1, // starting zoom
         });
 
-
-        map.addSource('1880', {
-            'type': 'geojson',
-            'data': geoData
+        map.addSource("1880", {
+            type: "geojson",
+            data: geoData,
         });
 
         map.addLayer({
-            'id': '1880',
-            'type': 'fill',
-            'source': '1880',
-            'paint': {
-                'fill-color': '#888888',
-                'fill-opacity': 0.4
-            }
+            id: "1880",
+            type: "fill",
+            source: "1880",
+            paint: {
+                "fill-color": "#888888",
+                "fill-opacity": 0.4,
+            },
         });
     });
 </script>
