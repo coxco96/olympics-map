dbc170e508fce20497ee60b293aa77bed3516e3d
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-07-17 23:37:37 -0400

    create page layout with map

--- map-app/src/routes/+page.svelte
+++ map-app/src/routes/+page.svelte
@@ -1,25 +1,50 @@
 <script>
     export let data;
-    import Map from './Map.svelte';
-    import { Container, Col, Row } from "@sveltestrap/sveltestrap";
-    console.log(data);
+    import Map from "./Map.svelte";
+    import { Container, Col, Row, Input } from "@sveltestrap/sveltestrap";
+    // console.log(data);
 </script>
 
 <main>
-    
-   <Container class='mt-5'>
+    <Container class="mt-5">
         <Row>
-            <Col md='6'>
+            <Col md="6">
                 <h1 class="display-4">Olympics Map</h1>
-                <p class="lead">
-                    Medals won etc etc
-                </p>
+                <p class="lead">Medals won etc etc</p>
+            </Col>
+        </Row>
+        <Row class='mb-3'>
+            <Col>
+                <Input type="select" placeholder="Select Sport">
+                    <option value="alpha">All sports</option>
+                    <option value="bravo">Bravo</option>
+                    <option value="charlie">Charlie</option>
+                </Input>
+            </Col>
+            <Col>
+                <Input type="select" placeholder="Select Year">
+                    <option value="alpha">All Years (1886-Present)</option>
+                    <option value="bravo">Bravo</option>
+                    <option value="charlie">Charlie</option>
+                </Input>
             </Col>
         </Row>
         <Row>
             <Col>
-            <Map/>
+                <Map />
             </Col>
+        </Row>
+        <Row>
+            <footer class='footer'>
+                <figure class="text-end">
+                    <blockquote class="blockquote">
+                      <p>I was built this way for a reason, so I'm going to use it.</p>
+                    </blockquote>
+                    <figcaption class="blockquote-footer">
+                     <cite title="Simone Biles">Simone Biles</cite>
+                    </figcaption>
+                  </figure>
+            </footer>
         </Row>
     </Container>
 </main>
--- map-app/src/routes/Map.svelte
+++ map-app/src/routes/Map.svelte
@@ -1,37 +1,41 @@
 <script>
-	// https://github.com/topojson/us-atlas
-	// https://github.com/d3/d3-geo
-	// https://observablehq.com/@mbostock/u-s-state-map
+    import { onMount } from "svelte";
+    import maplibregl from "maplibre-gl";
+    import "maplibre-gl/dist/maplibre-gl.css";
+    import geoData from '$lib/geojsons/world_1880.json';
 
-	
-	import { onMount } from 'svelte';
-	import * as topojson from 'topojson-client';
-	import { geoPath, geoMercator } from 'd3-geo';
-	import { draw } from 'svelte/transition';
-	
-	// https://github.com/topojson/us-atlas#us-atlas-topojson
-	const projection = geoMercator().scale(1300).translate([487.5, 305])
-	
-	const path = geoPath().projection(null);
-	
-	let countries = [];
-	// let counties = []
-	let mesh;
-	let selected;
-	//$: console.log({ selected })
-	
-	const points = [
-		{ lat: 38.421115245736, long: -82.44432596047203 },
-	].map(p => projection([p.long, p.lat]))
-	
-	onMount(async () => {
-		const world = await fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json')
-			.then(d => d.json())
-		console.log({ world })
 
-        // countries = topojson.feature(world).features;
-        // console.log(countries);
+    onMount(() => {
+        const map = new maplibregl.Map({
+            container: "map", // container id
+            // style: "https://demotiles.maplibre.org/style.json", // style URL
+            center: [0, 0], // starting position [lng, lat]
+            zoom: 1, // starting zoom
+        });
 
-	})
+
+        map.addSource('1880', {
+            'type': 'geojson',
+            'data': geoData
+        });
+
+        map.addLayer({
+            'id': '1880',
+            'type': 'fill',
+            'source': '1880',
+            'paint': {
+                'fill-color': '#888888',
+                'fill-opacity': 0.4
+            }
+        });
+    });
 </script>
 
+<div id="map"></div>
+
+<style>
+    #map {
+        height: 600px;
+        width: 100%;
+    }
+</style>
