2ac3b86cc7fda813159005f0ffa25bcc0b8285cc
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-07-17 22:13:32 -0400

    add historic geojsons

--- map-app/src/routes/+page.svelte
+++ map-app/src/routes/+page.svelte
@@ -1,11 +1,25 @@
 <script>
     export let data;
-
+    import Map from './Map.svelte';
+    import { Container, Col, Row } from "@sveltestrap/sveltestrap";
     console.log(data);
 </script>
 
-<h1>Olympics Map</h1>
-
-
-
-
+<main>
+    
+   <Container class='mt-5'>
+        <Row>
+            <Col md='6'>
+                <h1 class="display-4">Olympics Map</h1>
+                <p class="lead">
+                    Medals won etc etc
+                </p>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            <Map/>
+            </Col>
+        </Row>
+    </Container>
+</main>
---
+++ map-app/src/routes/Map.svelte
@@ -0,0 +1,37 @@
+<script>
+	// https://github.com/topojson/us-atlas
+	// https://github.com/d3/d3-geo
+	// https://observablehq.com/@mbostock/u-s-state-map
+
+	
+	import { onMount } from 'svelte';
+	import * as topojson from 'topojson-client';
+	import { geoPath, geoMercator } from 'd3-geo';
+	import { draw } from 'svelte/transition';
+	
+	// https://github.com/topojson/us-atlas#us-atlas-topojson
+	const projection = geoMercator().scale(1300).translate([487.5, 305])
+	
+	const path = geoPath().projection(null);
+	
+	let countries = [];
+	// let counties = []
+	let mesh;
+	let selected;
+	//$: console.log({ selected })
+	
+	const points = [
+		{ lat: 38.421115245736, long: -82.44432596047203 },
+	].map(p => projection([p.long, p.lat]))
+	
+	onMount(async () => {
+		const world = await fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json')
+			.then(d => d.json())
+		console.log({ world })
+
+        // countries = topojson.feature(world).features;
+        // console.log(countries);
+
+	})
+</script>
+
