86738c42889f09e29b70c354769182e5062a30ae
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-07-26 12:15:33 -0500

    yearsfilter save issue

--- map-app/src/lib/components/YearsFilter.svelte
+++ map-app/src/lib/components/YearsFilter.svelte
@@ -1,33 +1,13 @@
 <script>
-    import {getContext} from 'svelte';
     import {selectedYear, selectedSport, selectedEvent, filteredDataStore} from '$lib/utils/stores.js';
     import {yearsArray} from '$lib/utils/exports.js';
-    import {initialDataContext} from '$lib/utils/context.js';
-
-    // access unfiltered data
-    const initialData = getContext(initialDataContext);
-
-
-
-    let sport, year, sportEvent, filteredData;
+    let year = 'All years (1896-2024)';
+    let sport = 'All sports';
+    let sportEvent = 'All events';
+  
     $: selectedYear.subscribe(value => year = value);
     $: selectedSport.subscribe(value => sport = value);
     $: selectedEvent.subscribe(value => sportEvent = value);
-    $: filteredDataStore.subscribe(value => filteredData = value);
-
-    // $: console.log(`${year} from YearsFilter`)
-    // $: console.log(filteredData);
-
-    // Convert yearsArray to numbers and find the minimum
-    // let minYear = 1896;
-    // let maxYear = 2024;
-
-
-    // let steps = yearsArray
-    //     .map(y => +y) // convert to numbers
-    //     .filter(y => !isNaN(y)) // remove any non-numeric values
-    //     .sort((a, b) => a - b); // sort in ascending order
-
 
 
     function handleChange(event) {
@@ -35,28 +15,6 @@
     }
 
 </script>
-
-
-
-<!-- <div class="slider-container">
-    <label for="year-slider">Selected Year: {year}</label>
-    <input
-        type="range"
-        min={minYear}
-        max={maxYear}
-        step={1}
-        value={year}
-        on:input={handleChange}
-        class="form-range"
-        list="tickmarks"
-    />
-
-    <datalist id="tickmarks">
-        {#each steps as step}
-            <option value={step}>{step}</option>
-        {/each}
-    </datalist>
-</div> -->
 
 <select bind:value={year} on:change={handleChange} class="form-select" name="Year">
     {#each yearsArray.reverse() as year}
