948c6fc62dcef4a3b947a7436288b6771f2420a9
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-07-30 01:31:04 -0400

    undo config changes

--- map-app/svelte.config.js
+++ map-app/svelte.config.js
@@ -1,38 +1,11 @@
-// import adapter from '@sveltejs/adapter-vercel';
-
-// /** @type {import('@sveltejs/adapter-vercel').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: adapter(),
-// 	}
-// };
-
-// export default config;
-
-
 import adapter from '@sveltejs/adapter-vercel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/adapter-vercel').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-		vite: {
-			// Vite config here
-			plugins: [
-				// You can add other Vite plugins if needed
-			],
-			resolve: {
-				// Use @rollup/plugin-node-resolve for module resolution
-				plugins: [resolve()],
-			},
-			commonjs: {
-				// Use @rollup/plugin-commonjs to handle CommonJS modules
-				plugins: [commonjs()],
-			}
-		}
 	}
 };
 
 export default config;
+
