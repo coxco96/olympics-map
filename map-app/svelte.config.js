4741399c05ff822446b8656a9806affb2d266df9
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-07-30 01:29:50 -0400

    resolve rollup plugins

--- map-app/svelte.config.js
+++ map-app/svelte.config.js
@@ -1,11 +1,38 @@
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/adapter-vercel').Config} */
+// /** @type {import('@sveltejs/adapter-vercel').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+// 	}
+// };
+
+// export default config;
+
+
+import adapter from '@sveltejs/adapter-vercel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		vite: {
+			// Vite config here
+			plugins: [
+				// You can add other Vite plugins if needed
+			],
+			resolve: {
+				// Use @rollup/plugin-node-resolve for module resolution
+				plugins: [resolve()],
+			},
+			commonjs: {
+				// Use @rollup/plugin-commonjs to handle CommonJS modules
+				plugins: [commonjs()],
+			}
+		}
 	}
 };
 
 export default config;
-
