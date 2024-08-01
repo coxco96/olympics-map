ae4612cd744387cafeeb8c942874129c3d8dce21
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-08-01 00:20:20 -0400

    switch table sort button to sveltestrap component

--- map-app/src/lib/components/Table.svelte
+++ map-app/src/lib/components/Table.svelte
@@ -1,17 +1,17 @@
 <script>
-    import {Table} from '@sveltestrap/sveltestrap'
-    import { filteredDataStore, pointsTotalStore } from '$lib/utils/stores.js';
+    import { Table, Button } from "@sveltestrap/sveltestrap";
+    import { filteredDataStore, pointsTotalStore } from "$lib/utils/stores.js";
 
     // button toggle state
-    let sortBy = 'pointsTotal'; // default sorting option
+    let sortBy = "pointsTotal"; // default sorting option
 
     // subscribe to filterDataStore
     let filteredData;
     let pointsTotalArr;
-    $: filteredDataStore.subscribe(value => filteredData = value);
-    $: pointsTotalStore.subscribe(value => pointsTotalArr = value);
+    $: filteredDataStore.subscribe((value) => (filteredData = value));
+    $: pointsTotalStore.subscribe((value) => (pointsTotalArr = value));
 
-    function makeTableArr (filteredData, sortBy) {
+    function makeTableArr(filteredData, sortBy) {
         let tableArr = [];
         let countryData;
         for (let country in filteredData) {
@@ -20,68 +20,83 @@
             let bronzeCount = 0;
             let pointsTotal = 0;
             countryData = filteredData[country];
-            countryData.forEach(row => {
+            countryData.forEach((row) => {
                 // destructure row object to access medal
                 const { medal } = row;
-                
+
                 // count medals
-                if (medal === 'Gold') {
-                    goldCount++
+                if (medal === "Gold") {
+                    goldCount++;
                     pointsTotal += 3;
-                } else if (medal === 'Silver') {
-                    silverCount++
+                } else if (medal === "Silver") {
+                    silverCount++;
                     pointsTotal += 2;
-                } else if (medal ==='Bronze') {
-                    bronzeCount++
+                } else if (medal === "Bronze") {
+                    bronzeCount++;
                     pointsTotal += 1;
                 }
-
-            })
-            tableArr.push([country, goldCount, silverCount, bronzeCount, pointsTotal])
+            });
+            tableArr.push([
+                country,
+                goldCount,
+                silverCount,
+                bronzeCount,
+                pointsTotal,
+            ]);
         }
         // determine how to sort
-        if (sortBy === 'pointsTotal') {
-            tableArr.sort((a,b) => b[4] - a[4]) // sort by points
-        } else if (sortBy === 'country') {
-            tableArr.sort((a,b) => a[0].localeCompare(b[0])); // sort alphabetically by country
+        if (sortBy === "pointsTotal") {
+            tableArr.sort((a, b) => b[4] - a[4]); // sort by points
+        } else if (sortBy === "country") {
+            tableArr.sort((a, b) => a[0].localeCompare(b[0])); // sort alphabetically by country
         }
-            return tableArr;
+        return tableArr;
     }
 
     function toggleSort() {
-        sortBy = sortBy === 'pointsTotal' ? 'country' : 'pointsTotal';
+        sortBy = sortBy === "pointsTotal" ? "country" : "pointsTotal";
     }
 
-    $: buttonText = sortBy === 'pointsTotal' ? 'Sort by country' : 'Sort by results';
+    $: buttonText =
+        sortBy === "pointsTotal" ? "Sort alphabetically" : "Sort by rank";
 
     $: tableData = makeTableArr(filteredData, sortBy);
-
 </script>
 
 <div>
-    <button on:click={toggleSort}>{buttonText}</button>
+    <Button class="mt-2" color={"dark"} on:click={toggleSort}>
+        {buttonText}
+    </Button>
 </div>
-
 
 <!-- this is the sveltestrap Table component, not a self-reference -->
 <Table>
     <thead>
         <tr>
-            <th>Country</th>
-            <th>Gold Medals</th>
-            <th>Silver Medals</th>
-            <th>Bronze Medals</th>
+            <th class="th">Country</th>
+            <th class="th">Gold Medals</th>
+            <th class="th">Silver Medals</th>
+            <th class="th">Bronze Medals</th>
         </tr>
     </thead>
     <tbody>
-        {#each tableData as arr}
-        <tr>
-            {#each arr.slice(0,4) as x}
-            <td>{x}</td>
-            {/each}
-        </tr>
+        {#each tableData as arr, i}
+            <tr>
+                {#each arr.slice(0, 4) as x}
+                    <td>{x}</td>
+                {/each}
+            </tr>
         {/each}
-        
     </tbody>
 </Table>
 
+<style>
+    tbody {
+        font-size: 0.9rem;
+    }
+
+    .th {
+        text-transform: uppercase;
+        font-size: 0.85rem;
+    }
+</style>
